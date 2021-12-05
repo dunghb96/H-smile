59c46200964b897838be9d82d8bc355cf2d1390d
diemvt <diemvtph11370@fpt.edu.vn>
2021-12-05 21:34:39 +0700

    update-price

--- resources/views/frontend/price/index.blade.php
+++ resources/views/frontend/price/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <!--Start breadcrumb area-->
-<section class="breadcrumb-area" style="background-image: url(images/resources/breadcrumb-bg.jpg);">
+<section class="breadcrumb-area" style="background-image: url('{{ asset('images/resources/breadcrumb-bg.jpg') }}');">
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
@@ -23,7 +23,164 @@
 	</div>
 </section>
 <!--End breadcrumb area-->
+<br><br>
 
+<section class="pricing-plan-area style2">
+    <div class="container">
+        <div class="sec-title max-width text-center">
+            <h1>Bảng giá dịch vụ 1</h1>
+            <p>We are proud to have the opportunity to give you the smile of your dreams in affordable cost.</p>
+        </div>
+        <div class="row">
+            <!--Start single price box-->
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 text-center">
+                <div class="single-price-box">
+                    <div class="table-header">
+                        <div class="top">
+                            <h3>Optimal</h3>
+                            <span>*rates are subject to change</span>
+                        </div>
 
+                    </div>
+                    <div class="price-list">
+                        <ul>
+                            <li>Free Equipment Use</li>
+                            <li>24/7 Emergency Monitoring</li>
+                            <li>Senior Discount Available</li>
+                            <li>Free Monitoring for Spouse</li>
+                            <li>Monitored in the USA</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <!--End single price box-->
+            <!--Start single price box-->
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 text-center">
+                <div class="single-price-box">
+                    <div class="table-header">
+                        <div class="top">
+                            <h3>Premium</h3>
+                            <span>*rates are subject to change</span>
+                        </div>
+
+                    </div>
+                    <div class="price-list">
+                        <ul>
+                            <li>Free Equipment Use</li>
+                            <li>24/7 Emergency Monitoring</li>
+                            <li>Senior Discount Available</li>
+                            <li>Free Monitoring for Spouse</li>
+<li>Monitored in the USA</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <!--End single price box-->
+            <!--Start single price box-->
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 text-center">
+                <div class="single-price-box">
+                    <div class="table-header">
+                        <div class="top">
+                            <h3>Gold</h3>
+                            <span>*rates are subject to change</span>
+                        </div>
+
+                    </div>
+                    <div class="price-list">
+                        <ul>
+                            <li>Free Equipment Use</li>
+                            <li>24/7 Emergency Monitoring</li>
+                            <li>Senior Discount Available</li>
+                            <li>Free Monitoring for Spouse</li>
+                            <li>Monitored in the USA</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <!--End single price box-->
+        </div>
+    </div>
+</section>
+<section class="pricing-plan-area style2">
+    <div class="container">
+        <div class="sec-title max-width text-center">
+            <h1>Bảng giá dịch vụ 2</h1>
+            <p>We are proud to have the opportunity to give you the smile of your dreams in affordable cost.</p>
+        </div>
+        <div class="row">
+            <!--Start single price box-->
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 text-center">
+                <div class="single-price-box">
+                    <div class="table-header">
+                        <div class="top">
+                            <h3>Optimal</h3>
+                            <span>*rates are subject to change</span>
+                        </div>
+
+                    </div>
+                    <div class="price-list">
+                        <ul>
+                            <li>Free Equipment Use</li>
+                            <li>24/7 Emergency Monitoring</li>
+                            <li>Senior Discount Available</li>
+                            <li>Free Monitoring for Spouse</li>
+                            <li>Monitored in the USA</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <!--End single price box-->
+            <!--Start single price box-->
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 text-center">
+                <div class="single-price-box">
+                    <div class="table-header">
+                        <div class="top">
+                            <h3>Premium</h3>
+                            <span>*rates are subject to change</span>
+                        </div>
+
+                    </div>
+                    <div class="price-list">
+                        <ul>
+                            <li>Free Equipment Use</li>
+<li>24/7 Emergency Monitoring</li>
+                            <li>Senior Discount Available</li>
+                            <li>Free Monitoring for Spouse</li>
+                            <li>Monitored in the USA</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <!--End single price box-->
+            <!--Start single price box-->
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 text-center">
+                <div class="single-price-box">
+                    <div class="table-header">
+                        <div class="top">
+                            <h3>Gold</h3>
+                            <span>*rates are subject to change</span>
+                        </div>
+
+                    </div>
+                    <div class="price-list">
+                        <ul>
+                            <li>Free Equipment Use</li>
+                            <li>24/7 Emergency Monitoring</li>
+                            <li>Senior Discount Available</li>
+                            <li>Free Monitoring for Spouse</li>
+                            <li>Monitored in the USA</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <!--End single price box-->
+        </div>
+    </div>
+</section>
 @endsection
-
