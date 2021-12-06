b1756419762c17dc7f4d6a52e06afe3facaf1788
dunglk <ledunghb96@gmail.com>
2021-12-06 21:43:57 +0700

    add layout

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -1,218 +1,44 @@
 @extends('frontend.layout.index')
 @section('content')
-    <main id="main">
 
-        <!-- ======= Breadcrumbs Section ======= -->
-        <section class="breadcrumbs">
+@include('frontend.layout.breadcrumb')
 
-        </section><!-- End Breadcrumbs Section -->
-
-        <section class="inner-page">
-            <div class="container">
-                <div class="section-title" style="margin-bottom: 70px;">
-                    <h2>Giới thiệu về H-Smile</h2>
-                    <p>
-                        Nha khoa thẩm mỹ quốc tế H-Smile được thành lập năm 2021, với tôn chỉ giúp đỡ khách hàng thay
-                        đổi cuộc sống thông qua nụ cười mới mang lại hạnh phúc và sự thành công. H-Smile quan tâm và chú
-                        trọng đến vấn đề sức khỏe và tính thẩm mỹ cao trong dịch vụ nha khoa. Chúng tôi cam kết tư vấn
-                        thật tâm, hiệu quả và cung cấp dịch vụ chất lượng hoàn hảo đem lại sự trải nghiệm tuyệt vời nhất
-                        cho khách hàng trên mỗi hệ thống của nha khoa H-Smile.
-                    </p>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col">
-                        <img src="{{ asset('frontend/assets/img/gallery/a-1.jpg') }}" alt="" width="500px" srcset="">
-
-                        {{-- <!-- <div class="w3-content w3-section" style="max-width:500px;">
-                        <img class="mySlides" src="{{asset('frontend/assets/img/gallery/gallery-1.jpg')}}" style="width:100%">
-                        <img class="mySlides" src="{{asset('frontend/assets/img/gallery/gallery-2.jpg')}}" style="width:100%">
-                        <img class="mySlides" src="{{asset('frontend/assets/img/gallery/gallery-3.jpg')}}" style="width:100%">
-                    </div> --> --}}
-                    </div>
-                    <div class="col">
-                        <h3 style="color:#1977cc"> H-Smile - Thành công từ những nụ cười. <br></h3>
-                        <p><strong>H-Smile hiểu rằng, mỗi khách hàng đến với chúng tôi luôn có 1 câu chuyện riêng, một
-                                nhu cầu và mong muốn riêng. </strong> <br> <br>
-                            Thấu hiểu được tâm lý đó H-Smile lựa chọn giải pháp tối ưu cho khách hàng trong quá trình tư
-                            vấn nha khoa thẩm mỹ.
-                            Đặc biệt, chúng tôi tự hào rằng các kỹ thuật thẩm mỹ nha của H-Smile được cập nhật thường
-                            xuyên, là công nghệ tiên tiến
-                            và mới nhất trên thế giời, bảo toàn răng gốc tối đa, an toàn, bêng vững theo thời gian. Hữu
-                            xạ tự nhiên hương đó cũng là lý
-                            do vì sao H-Smile lại có tỷ lệ giới thiệu khách hàng từ khách hàng cũ cao nhất trong giới
-                            nha khoa thẩm mỹ.</p>
-                    </div>
-                </div>
-
-
-            </div>
-        </section>
-        <section class="" style="text-align: center;">
-            <div class="container">
-                <div class="section-title" id="text-1">
-                    <h2 style="color:#1977cc">Giá trị cốt lõi</h2>
-                    <p> <strong>- Sứ mệnh:</strong> Nha khoa H-Smile cung cấp các dịch vụ chăm sóc và điều trị nha khoa
-                        chất
-                        lượng cao, đem lại hiệu quả điều trị: ĐẸP VỀ THẨM MỸ – TỐT VỀ CHỨC NĂNG ĂN NHAI và MỘT SỨC KHỎE
-                        LÂU DÀI cho bệnh
-                        nhân.</p> <br>
-                    <p> <strong>- Tầm nhìn:</strong> H-Smile mong muốn bảo tồn giá trị cốt lõi ngành nha trở thành hệ
-                        thống nha
-                        khoa thẩm mỹ duy nhất Việt Nam mang đến dịch vụ an toàn nhất với phong cách phục vụ chuyên
-                        nghiệp hàng đầu
-                        cho người Việt.</p> <br>
-                    <p> <strong>- Giá trị văn hóa:</strong> Mỗi thành viên của Nha khoa H-Smile đều thực hiện phương
-                        châm:
-                        Tôn trọng khách hàng, tôn trọng tập thể, đồng nghiệp; Tận tâm chăm sóc và điều trị cho bệnh
-                        nhân; Uy
-                        tín, trung thực trong công việc; Chuyên nghiệp trong tư vấn, chăm sóc, điều trị.</p> <br>
-                    <p><strong>- Giá trị cạnh tranh:</strong>Đội ngũ bác sĩ chuyên gia cùng hệ thống trang thiết bị nha
-                        khoa hiện đại đem đến sự hài lòng về kết quả điều trị, sức khỏe lâu dài. Điều đó sẽ khiến khách
-                        hàng
-                        đưa Nha khoa H-Smile đến với cộng đồng.</p> <br>
-                    <p> <strong>- Nha khoa H-Smile cam kết:</strong> Bác sĩ chuyên gia khám và điều trị tận tâm; Hệ
-                        thống
-                        trang thiết bị y tế chuẩn quốc tế; Dịch vụ nha khoa uy tín; Chất lượng hàng đầu</p> <br>
-                    <img src="{{ asset('frontend/assets/img/gallery/a-2.jpg') }}" alt="" style="margin-bottom: 130px;">
-                    <h3 style="color:#1977cc;text-align: left; ">Tại sao chọn chúng tôi ?</h3>
-                    <p>Chúng tôi sẽ giúp bạn tìm lại nụ cười rạng rỡ. <br>
-                        Chương trình điều trị độc quyền nha khoa H-Smile cam kết mạng lại cho bạn một nụ cười tự nhiên
-                        rạng rỡ nhất.phương
-                        pháp này sử dụng một chuỗi hệ thống vi tính hóa giúp giảm thiểu những trị liệu xâm lấn. Kết hợp
-                        với kinh nghiệm và bí quyết từ
-                        đội ngũ chuyên gia sẽ mang đến cho bạn một nụ cười đẹp bền lâu. Chúng tôi cảm thấy rất tự hào
-                        khi có thể giúp đỡ các bệnh nhân
-                        của mình lấy lại sự tự tin khi sở hữu một nụ cười duyên dáng. Bạn sẽ thấy được rằng chúng tôi
-                        không bỏ xót bất kỳ chi tiết nào
-                        trong công đoạn phục hình để đem lại cho bạn nụ cười hằng mong ước.
-                    </p> <br>
-                    <h3 style="color:#1977cc;text-align: left; ">Chúng tôi sử dụng vật liệu và thiết bị nha khoa chất
-                        lượng quốc tế tốt nhất</h3>
-                    <p>Với trang thiết bị hiện đại được nhập khẩu từ những nền y tế phát triển như Mỹ, Nhật, Châu Âu...
-                        Nha Khoa H-Smile mang đến trải nghiệm cao cấp cho khách hàng. Cùng với số
-                        lượng dân cư, chất lượng cuộc sống không ngừng tăng cao, nha khoa H-Smile hoàn toàn có thể đáp
-                        ứng nhu cầu chăm sóc sức khỏe răng miệng, làm đẹp, thẩm mỹ nha khoa cao cấp cho cộng đồng với
-                        chi phí hợp lý.Đội ngũ bác sĩ, kĩ thuật viên được đào tạo chuyên sâu, phong cách phục vụ chu
-                        đáo, tận tình tư
-                        vấn cho khách hàng có thể lựa chọn dịch vụ phù hợp, tốt nhất.
-                    </p>
-                    <br>
-                    <h3 style="color:#1977cc;text-align: left; ">Chất lượng dịch vụ nha khoa</h3>
-                    <p>Trong tất cả các dịch vụ nha khoa, H-Smile luôn áp dụng điều trị theo quy trình chuẩn, đảm bảo an
-                        toàn và hiệu quả. Chế độ chăm sóc sau điều trị được H-Smile quan tâm chú trọng đặc biệt để tạo
-                        ra cảm giác thoải mái và dễ chịu nhất cho bệnh nhân và khách hàng.Trong quá trình điều trị, nha
-                        khoa H-Smile đảm bảo các thao tác đều được thực hiện chính xác trong môi trường vô trùng tuyệt
-                        đối, hạn chế gây đau đớn tới mức tối đa, áp dụng các qui trình khử trùng đảm bảo nhất hiện nay
-                        với các máy móc nhập khẩu từ Mỹ và châu Âu.
-                    </p>
+<section class="about-area home2">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="about-image-holder wow fadeInLeft animated" data-wow-delay="900ms" style="visibility: visible; animation-delay: 900ms; animation-name: fadeInLeft;">
+                    <img src="images/resources/about.jpg" alt="Awesome Image">
                 </div>
             </div>
-
-        </section>
-
-
-        <section id="testimonials" class="testimonials">
-            <div class="container">
-                <div class="section-title">
-                    <h2>Khách hàng nói gì về H-Smile</h2>
-                </div>
-                <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-                    <div class="swiper-wrapper">
-
-                        <div class="swiper-slide">
-                            <div class="testimonial-wrap">
-                                <div class="testimonial-item">
-                                    <img src="{{ asset('frontend/assets/img/testimonials/hoang-phi-huyen.png') }}"
-                                        class="testimonial-img" alt="">
-                                    <h3>Hoàng Phi Huyền</h3>
-                                    <h4>Boss Huyền Phi Comestic</h4>
-                                    <p>
-                                        <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                        Răng của Huyền trước đây khá đều, tuy nhiên lại không được trắng sáng. Dù không
-                                        tự ti nhưng là 1
-                                        Boss của hệ thống hơn 1000 người thì hình ảnh nụ cười rạng rỡ chính là yếu tố
-                                        khiến Huyền luôn được
-                                        yêu mến và tin tưởng.
-                                        <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                                    </p>
-                                </div>
-                            </div>
-                        </div><!-- End testimonial item -->
-
-                        <div class="swiper-slide">
-                            <div class="testimonial-wrap">
-                                <div class="testimonial-item">
-                                    <img src="{{ asset('frontend/assets/img/testimonials/nguyen-tra-giang3.png') }}"
-                                        class="testimonial-img" alt="">
-                                    <h3>Nguyễn Trà Giang</h3>
-                                    <h4>Boss Mint beauty</h4>
-                                    <p>
-                                        <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                        Răng của Giang trước đây gặp rất nhiều vấn đề, răng khấp khểnh, cười hở lợi và
-                                        không được trắng sáng.
-                                        <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                                    </p>
-                                </div>
-                            </div>
-                        </div><!-- End testimonial item -->
-
-                        <div class="swiper-slide">
-                            <div class="testimonial-wrap">
-                                <div class="testimonial-item">
-                                    <img src="{{ asset('frontend/assets/img/testimonials/nguyen-quyen.png') }}"
-                                        class="testimonial-img" alt="">
-                                    <h3>Nguyễn Quyên</h3>
-                                    <h4>Boss Mỹ phẩm Herrin Care</h4>
-                                    <p>
-                                        <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                        Bây giờ mình luôn tự tin , thoải mái mỗi khi cười và nhận được rất nhiều lời
-                                        khen từ bạn bè và đối tác.
-                                        <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                                    </p>
-                                </div>
-                            </div>
-                        </div><!-- End testimonial item -->
-
-                        <div class="swiper-slide">
-                            <div class="testimonial-wrap">
-                                <div class="testimonial-item">
-                                    <img src="{{ asset('frontend/assets/img/testimonials/tung-min.png') }}"
-                                        class="testimonial-img" alt="">
-                                    <h3>Tùng Min</h3>
-                                    <h4>Diễn viên</h4>
-                                    <p>
-                                        <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                        Một người khó tính và sợ đau như Tùng mà sau khi bọc răng sứ tại Win Smile còn
-                                        cảm thấy hài lòng thì Tùng nghĩ dịch vụ tại Win Smile rất đáng để trải nghiệm.
-                                        <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                                    </p>
-                                </div>
-                            </div>
-                        </div><!-- End testimonial item -->
-
-                        <div class="swiper-slide">
-                            <div class="testimonial-wrap">
-                                <div class="testimonial-item">
-                                    <img src="{{ asset('frontend/assets/img/testimonials/thuong-cin-2.png') }}"
-                                        class="testimonial-img" alt="">
-                                    <h3>Thương Cin</h3>
-                                    <h4>Diễn viên</h4>
-                                    <p>
-                                        <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                        Làm xong cũng chẳng đau gì chỉ thấy đẹp thôi, mình tự tin hát và trước ống kính
-                                        lắm rồi.
-                                        <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                                    </p>
-                                </div>
-                            </div>
-                        </div><!-- End testimonial item -->
-
+            <div class="col-xl-6">
+                <div class="inner-content">
+                    <div class="sec-title">
+                        <h1>Nhiệm vụ về nha khoa H-Smile của chúng tôi</h1>
                     </div>
-                    <div class="swiper-pagination"></div>
+                    <div class="about-text-holder">
+                        <p>H-Smile được bắt đầu vào năm 1995 như một phòng khám nha khoa tư nhân nhỏ ở Binghamton, NY, Hoa Kỳ. Tìm kiếm dịch vụ chăm sóc nha khoa giá cả phải chăng?</p>
+                        <p>Lấy một ví dụ tầm thường, ai trong chúng ta từng thực hiện tập thể dục tốn nhiều công sức, ngoại trừ để có được một số lợi thế từ nó? Nhưng ai có quyền tìm ra lỗi lầm với một người đàn ông chọn tận hưởng một niềm vui không có hậu quả khó chịu, nỗi đau dẫn đến niềm vui ca ngợi những lời dạy của nhà thám hiểm vĩ đại...</p>
+                        <div class="author-box fix">
+                            <div class="img-box">
+                                <img src="images/resources/ceo.png" alt="Awesome Image">
+                            </div>
+                            <div class="text-box">
+                                <h3>Dr. Jerome Sinclair</h3>
+                                <span>CEO &amp; Founder</span>
+                            </div>
+                            <div class="signatire-box">
+                                <img src="images/resources/signature.png" alt="Signature">
+                            </div>
+                        </div>
+                        <div class="read-more">
+                            <a class="btn-two" href="#"><span class="flaticon-next"></span>Xem thêm về chúng tôi</a>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-        </section>
-    </main>
+        </div>
+    </div>
+</section>
+
 @endsection
