537e67f52e373dd39cf77c0687b43adb71103022
hungduy3n2k1 <hungduy3n2k1@gmail.com>
2021-12-11 22:30:11 +0700

    Revert "new update"
    This reverts commit 768ba034af1327c812b652235320a9dad3a64a59.

--- resources/views/backend/layout/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="sidenav-overlay"></div>
-<div class="drag-target"></div>
-<!-- BEGIN: Footer-->
-<footer class="footer footer-static footer-light">
-    <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2021<a class="ml-25" href="https://velo.vn" target="_blank" style="color:#12059b !important">H-Smile</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span><span class="float-md-right d-none d-md-block" style="font-weight:bold; color:red">HOTLINE Hỗ trợ 0123456789 <i data-feather="heart"></i></span></p>
-</footer>
-<button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
