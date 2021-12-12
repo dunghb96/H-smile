5ef0753dde8ed2014aa5d2881d00c6a1d39e547d
dunglk <ledunghb96@gmail.com>
2021-12-12 14:53:23 +0700

    sửa font

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <title>Home Two || Dento || Responsive HTML 5 Template</title>
+    <title>H-Smile</title>
 
     <!-- responsive meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,7 +19,10 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/frontend/images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/frontend/images/favicon/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/frontend/images/favicon/favicon-16x16.png" sizes="16x16">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&amp ;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@400;500&display=swap" rel="stylesheet">
     <!-- Fixing Internet Explorer-->
     <!--[if lt IE 9]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
