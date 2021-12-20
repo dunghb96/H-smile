5fb3521c66df7c3c541cde29ddcffafd7778ba68
changanuong1742 <35423451+changanuong1742@users.noreply.github.com>
2021-12-20 11:45:00 +0700

    config senmail

--- resources/views/frontend/mail/sendmail.blade.php
+++ resources/views/frontend/mail/sendmail.blade.php
@@ -1,35 +1,77 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Page Title</title>
     <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
+        body {
+            margin-top: 20px;
         }
 
-        td {
+        .mail-seccess {
             text-align: center;
+            background: #fff;
+            border-top: 1px solid #eee;
+        }
+
+        .mail-seccess .success-inner {
+            display: inline-block;
+        }
+
+        .mail-seccess .success-inner h1 {
+            font-size: 100px;
+            text-shadow: 3px 5px 2px #3333;
+            color: #006DFE;
+            font-weight: 700;
+        }
+
+        .mail-seccess .success-inner h1 span {
+            display: block;
+            font-size: 25px;
+            color: #333;
+            font-weight: 600;
+            text-shadow: none;
+            margin-top: 20px;
+        }
+
+        .mail-seccess .success-inner p {
+            padding: 20px 15px;
+        }
+
+        .mail-seccess .success-inner .btn {
+            color: #fff;
         }
     </style>
 </head>
-
 <body>
 
-    <h5> Dear: {{$name}}!
-    </h5>
-    <p>Có yêu cầu đặt lịch</p>
-    </p>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+<section class="mail-seccess section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 col-12">
+                <!-- Error Inner -->
+                <div class="success-inner">
+                    <h1><i class="fa fa-envelope"></i><span>Yêu cầu đặt lịch thành công!</span></h1>
+                    <p>Xin chào, {{$name}}</p>
+                    <p>Cảm ơn bạn đã tin tưởng dịch vụ của chung tôi, yêu cầu đặt lịch của bạn đã được tiếp nhận. Nhân
+                        viên tư vấn sẽ gọi lại và xác minh lịch khám của mình một lần nữa</p>
+
+                    <p>Thông tin yêu cầu đặt lịch</p>
+                    <ul>
+                        <li>Dịch vụ: {{$serviceSelected}}</li>
+                        <li>Thời gian: {{ $dateSelected }}</li>
+                        <li>Ca khám: {{ $shiftName }}</li>
+                        <li>Tình trạng: {{$descRequest}}</li>
+                    </ul>
+                    <p class="font-weight-bold">Cảm ơn quý khách</p>
+
+
+                </div>
+                <!--/ End Error Inner -->
+            </div>
+        </div>
+    </div>
+</section>
 
 </body>
-
 </html>
