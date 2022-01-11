ed37692e8dfa64c17d702a044639d66f3daa6f2d
dunglk <ledunghb96@gmail.com>
2022-01-12 00:51:16 +0700

    up model and migration

---
+++ database/migrations/2022_01_11_222542_create_prescriptions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrescriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('schedule_id');
+            $table->string('medicine_name');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+}
